bd7f34de0f9f5c79f33c8ec83ec5838d99099dba
Michael G. Noll <michael@michael-noll.com>
2018-04-21 03:31:45 +0200

    Add comments

--- app.go
+++ app.go
@@ -7,10 +7,12 @@ import (
 	"net/http"
 )
 
+// Response is just a very basic example.
 type Response struct {
 	Status string `json:"status,omitempty"`
 }
 
+// GetStatus returns always the same response.
 func GetStatus(w http.ResponseWriter, _ *http.Request) {
 	b := Response{Status: "idle"}
 	json.NewEncoder(w).Encode(b)
