02a1f113b9f843b67c1758729a11d1e3c15bd11b
Michael G. Noll <miguno@miguno.com>
2023-01-18 20:20:49 +0100

    Quote variables

--- create_image.sh
+++ create_image.sh
@@ -4,4 +4,4 @@ declare -r IMAGE_NAME="miguno/golang-docker-build-tutorial"
 declare -r IMAGE_TAG="latest"
 
 echo "Building image '$IMAGE_NAME:$IMAGE_TAG'..."
-docker build -t $IMAGE_NAME:$IMAGE_TAG .
+docker build -t "$IMAGE_NAME":"$IMAGE_TAG" .
