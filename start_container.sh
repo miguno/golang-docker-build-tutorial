2ea6674cf73902dccd42bac53a1221136e2c94d2
Michael G. Noll <miguno@miguno.com>
2023-01-18 20:21:43 +0100

    Improve setup of bash scripts

--- start_container.sh
+++ start_container.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+# `-u`: Errors if a variable is referenced before being set
+# `-o pipefail`: Prevent errors in a pipeline (`|`) from being masked
+set -uo pipefail
 
 declare -r IMAGE_NAME="miguno/golang-docker-build-tutorial"
 declare -r IMAGE_TAG="latest"
