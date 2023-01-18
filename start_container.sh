25d66d29a6390063edb08ddadb2fc2c3b78d80ff
Michael G. Noll <miguno@miguno.com>
2023-01-18 20:20:27 +0100

    Quote variables

--- start_container.sh
+++ start_container.sh
@@ -5,5 +5,5 @@ declare -r IMAGE_TAG="latest"
 declare -r APP_PORT="8123"
 
 echo "Starting container for image '$IMAGE_NAME:$IMAGE_TAG', exposing port $APP_PORT/tcp"
-docker run -p $APP_PORT:$APP_PORT $IMAGE_NAME:$IMAGE_TAG
+docker run -p "$APP_PORT":"$APP_PORT" "$IMAGE_NAME":"$IMAGE_TAG"
 
